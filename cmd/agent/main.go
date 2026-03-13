a66aea9c0647905d04d200af7c137bfe7e595809
aleksej <grigorenko.aleks@gmail.com>
2026-03-13 13:48:58 +0500

    Инкремент 7 Дополните API сервера, чтобы позволить ему принимать метрики в формате JSON.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -157,10 +159,50 @@ func (m *Metrics) ReportWithBaseURL(baseURL string) {
 	m.mu.RUnlock()
 
 	for name, value := range gauges {
-		m.SendMetricWithClient(client, baseURL, "gauge", name, value)
+		metric := model.Metrics{
+			ID:    name,
+			MType: "gauge",
+			Value: &value,
+		}
+		m.sendJSON(baseURL, metric)
 	}
 	for name, value := range counters {
-		m.SendMetricWithClient(client, baseURL, "counter", name, value)
+		delta := value
+		metric := model.Metrics{
+			ID:    name,
+			MType: "counter",
+			Delta: &delta,
+		}
+		m.sendJSON(baseURL, metric)
+	}
+}
+
+// sendJSON отправляет одну метрику в формате JSON
+func (m *Metrics) sendJSON(baseURL string, metric model.Metrics) {
+	data, err := json.Marshal(metric)
+	if err != nil {
+		fmt.Printf("Error marshaling metric %s: %v\n", metric.ID, err)
+		return
+	}
+
+	url := fmt.Sprintf("%s/update", baseURL)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Printf("Error creating request for %s: %v\n", metric.ID, err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending metric %s: %v\n", metric.ID, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error response for %s: %s\n", metric.ID, resp.Status)
 	}
 }
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -87,6 +87,8 @@ func main() {
 	r.Use(noDoubleSlashes)
 
 	// Маршруты
+	r.Post("/update", handler.UpdateJSONHandler)
+	r.Post("/value", handler.GetValueJSONHandler)
 	r.Post("/update/{type}/{name}/{value}", handler.UpdateHandler)
 	r.Get("/value/{type}/{name}", handler.GetValueHandler)
 	r.Get("/", handler.ListMetricsHandler)
