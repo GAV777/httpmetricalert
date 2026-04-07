69141c2056ee24bbc66f487173e95ed42efdae3a
aleksej <grigorenko.aleks@gmail.com>
2026-04-07 23:21:33 +0500

    Инкремент 13

--- internal/migration/20260407120000_init.down.sql
+++
@@ -1,6 +0,0 @@
--- Миграция 001: Откат создания таблиц
-
-DROP INDEX IF EXISTS idx_counters_name;
-DROP INDEX IF EXISTS idx_gauges_name;
-DROP TABLE IF EXISTS counters;
-DROP TABLE IF EXISTS gauges;
